36d3c8bcd54963c9bec57c1ecc4fb3ab0942e8b2
wupuyi <305652984@qq.com>
2018-08-02 11:24:48 +0800

    搭建项目

---
+++ src/components/vue-echart-wordcloud.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="chart-container">
+
+  </div>
+</template>
+
+<script>
+import echarts from "echarts/lib/echarts";
+import "echarts/chart/wordCloud";
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.chart-container {
+  
+}
+</style>
